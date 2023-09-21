07bd7a32696bdb2d520e620c5361e02219b7246f
Saleem-ASD <saleem.amiri@student.uts.edu.au>
2023-09-21 22:27:23 +1000

    Admin page and Index

---
+++ asdproject/src/Admin.js
@@ -0,0 +1,17 @@
+
+// src/Admin.js
+
+import React from 'react';
+
+const Admin = () => {
+  return (
+    <div>
+      <h1>Restaurant Admin Dashboard</h1>
+      <p>Welcome, Admin! Here you can manage your restaurant's operations.</p>
+      <button>View Orders</button>
+      <button>Manage Menu</button>
+    </div>
+  );
+}
+
+export default Admin;
--- asdproject/src/App.js
+++ asdproject/src/App.js
@@ -1,22 +1,29 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Admin from './Admin';
 
 function App() {
+  const [isAdminView, setAdminView] = useState(false);
+
+  if (isAdminView) {
+    return <Admin />;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Welcome to Sopranos</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        
         </a>
+        <button onClick={() => setAdminView(true)}>Go to Admin Dashboard</button>
       </header>
     </div>
   );
